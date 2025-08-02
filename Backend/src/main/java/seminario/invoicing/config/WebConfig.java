691f1c622b5d58d16724d04a11f2217d8049c3b5
Andrés <anfebeba2001@gmail.com>
2025-08-01 20:41:51 -0500

    adding frontend functions for product

--- Backend/src/main/java/seminario/invoicing/config/WebConfig.java
+++ Backend/src/main/java/seminario/invoicing/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/v1/api/**")
                         .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*");
             }
         };
